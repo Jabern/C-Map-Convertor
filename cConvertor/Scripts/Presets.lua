4d6f8f3b7c1bd1701b35069afc2b94e56bd464f2
CobraCNC <62215124+CobraCNC@users.noreply.github.com>
2020-03-15 13:38:49 -0400

    cStream commit
    Converts to cStream usage

---
+++ cConvertor/Scripts/Presets.lua
@@ -0,0 +1,12 @@
+meta = {}
+
+-- This preps the meta file
+table.insert(meta,'<meta>')
+table.insert(meta,'	<info type="script" name="CStream" author="CobraJ" description="cStream version 1" version="1" cStream="1" />')
+
+table.insert(meta,'	<file src="gta3.JSD" />')
+table.insert(meta,' ') -- Blank Space
+table.insert(meta,' ') -- Blank Space	
+table.insert(meta,'	<script src="Settings/CWaterData.lua" type="client" />')
+table.insert(meta,' ') -- Blank Space
+table.insert(meta,' ')
