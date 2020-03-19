08b92a1915d863a261a4ea82a4e7cdf428bd7276
Jaber The BobCatTeam <jaberlama123@gmail.com>
2020-03-19 07:11:30 +0200

    Update Presets.lua
    Fixing a little mistake in the presets, also proposing to add the download priority line because the actual CStream tries to replace the objects even if the client still didn't download all the content files.

--- cConvertor/Scripts/Presets.lua
+++ cConvertor/Scripts/Presets.lua
@@ -4,9 +4,10 @@ meta = {}
 table.insert(meta,'<meta>')
 table.insert(meta,'	<info type="script" name="CStream" author="CobraJ" description="cStream version 1" version="1" cStream="1" />')
 
-table.insert(meta,'	<file src="gta3.JSD" />')
+table.insert(meta,'	<file src="gta3.CSD" />')
 table.insert(meta,' ') -- Blank Space
 table.insert(meta,' ') -- Blank Space	
 table.insert(meta,'	<script src="Settings/CWaterData.lua" type="client" />')
 table.insert(meta,' ') -- Blank Space
-table.insert(meta,' ')
+table.insert(meta,' ') -- Blank Space
+table.insert(meta,' <download_priority_group>1</download_priority_group>')
